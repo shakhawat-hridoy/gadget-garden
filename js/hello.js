6acaa130fd5b6776c922d8ea45c7686f99d5e2a8
shakhawat-hridoy <shakhawat.hridoy56@gmail.com>
2022-02-28 23:33:51 +0600

    show text on page

--- js/hello.js
+++ js/hello.js
@@ -3,7 +3,20 @@ console.log('abc');
 const loadPhone = () =>{
     fetch('https://openapi.programming-hero.com/api/phones?search=iphone')
     .then(response=>response.json())
-    .then(data=>console.log(data))
+    .then(data=>showPhone(data))
 }
 
-loadPhone();
+//loadPhone();
+
+const showPhone= (data) =>{
+    // console.log(data);
+    console.log(data.data[0].brand);
+
+    const div = document.getElementById('search-result');
+
+    const p = document.createElement('p');
+    p.innerText = `Name of the phone is: ${data.data[0].brand}`;
+
+    div.appendChild(p);
+}
+
