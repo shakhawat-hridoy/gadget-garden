933f4de0ca3350881c94d40cd16820f9499ec0df
shakhawat-hridoy <shakhawat.hridoy56@gmail.com>
2022-03-01 11:08:34 +0600

    functional card button

--- js/hello.js
+++ js/hello.js
@@ -1,33 +1,39 @@
-console.log('abc');
-
 const loadPhone = () =>{
     fetch('https://openapi.programming-hero.com/api/phones?search=iphone')
     .then(response=>response.json())
     .then(phoneList=>showPhone(phoneList.data))
 }
+loadPhone();
 
-//loadPhone();
+const specificPhone=(id)=>{
+    fetch(`https://openapi.programming-hero.com/api/phone/${id}`)
+    .then(response=>response.json())
+    .then(onePhone=>console.log(onePhone))
+}
 
 const showPhone= (phones) =>{
     console.log(phones);
-    console.log(phones[0].brand);
 
     for(phone of phones){
-        const div = document.getElementById('my-card');
 
+    const div = document.getElementById('my-card');
     const cardDiv = document.createElement('div');
     cardDiv.innerHTML = 
     `
     <div class="col">
-      <div class="card">
-        <img src=${phone.image} class="card-img-top" alt="">
+      <div class="card p-4">
+        <img class="w-50 h-50" src=${phone.image} class="card-img-top" alt="">
 
         <div class="card-body">
-            <h5 class="card-title">Model: ${phone.phone_name}</h5>
-            <h3>Brand: ${phone.brand}</h3>
+            <h3 class="card-title">Model: ${phone.phone_name}</h3>
+
+            <h5>Brand: ${phone.brand}</h5>
+
+            <button onclick="specificPhone(phone.slug)" type="button" class="btn btn-primary">Details</button>
+
         </div>
       </div>
-    </div> `;
+    </div>  `;
     div.appendChild(cardDiv);
     }
 
