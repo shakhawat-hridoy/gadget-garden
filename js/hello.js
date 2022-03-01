2f27c01063618028a7b8c18470ec9a36f0496b29
shakhawat-hridoy <shakhawat.hridoy56@gmail.com>
2022-03-01 14:08:54 +0600

    no phone message and case avoidance

--- js/hello.js
+++ js/hello.js
@@ -1,62 +1,77 @@
+// Take input from user
 const phoneInput=()=>{
-  const brandName= document.getElementById('input-brand').value;
-  loadPhone(brandName);
+  const brandName=document.getElementById('input-brand').value;
+  const universalBrandName = brandName.toLowerCase();
+  loadPhone(universalBrandName);
 }
 
+//Bring requested phones by brand
 const loadPhone = (searchText) =>{
-    // fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`)
-    fetch(`https://openapi.programming-hero.com/api/phones?search=oppo`)
+    fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`)
+    // fetch(`https://openapi.programming-hero.com/api/phones?search=oppo`)
     .then(response=>response.json())
     .then(phoneList=>showPhone(phoneList.data))
 }
-loadPhone();
+//loadPhone();
 
-const showDetails=(phoneSpecifications)=>{
-  const parenttDiv= document.getElementById('phone-details');
-
-  const childDiv= document.createElement('div');
-
-  console.log(phoneSpecifications);
-  console.log(phoneSpecifications.releaseDate);
-  console.log(phoneSpecifications.slug);
-
-  childDiv.innerHTML=
-  `<img class="h-25 w-25" src=${phoneSpecifications.image}></img>
-  <p>'Name of phone: ${phoneSpecifications.slug}'</p>
-  <p>'Release date: ${phoneSpecifications.releaseDate}'</p>
-  `;
-  parenttDiv.appendChild(childDiv);
-}
-
-const selectPhone=(id)=>{
-    fetch(`https://openapi.programming-hero.com/api/phone/${id}`)
-    .then(response=>response.json())
-    .then(onePhone=>showDetails(onePhone.data))
-}
-
+//Show available phones
 const showPhone= (phones) =>{
+  if(phones.length!=0){
     for(phone of phones){
       phoneId= phone.slug;
     const div = document.getElementById('my-card');
     const cardDiv = document.createElement('div');
+  
+    //dynamic card generation
     cardDiv.innerHTML = 
     `
     <div class="col">
       <div class="card p-4">
         <img class="w-25 h-25" src=${phone.image} class="card-img-top" alt="">
-
         <div class="card-body">
             <h3 class="card-title">Model: ${phone.phone_name}</h3>
-
             <h5>Brand: ${phone.brand}</h5>
-
             <button onclick="selectPhone('${phoneId}')" type="" class="btn btn-primary align-middle">Details2</button>
-
-
         </div>
       </div>
-    </div>  
-    `;
+    </div> `;
     div.appendChild(cardDiv);
     }
+  }
+    else{
+      alert('no phone found');
+    }
+}
+
+//Select ome phone from the card
+const selectPhone=(id)=>{
+  fetch(`https://openapi.programming-hero.com/api/phone/${id}`)
+  .then(response=>response.json())
+  .then(onePhone=>showDetails(onePhone.data))
+}
+
+//Show the details of one specific phone
+const showDetails=(phoneSpecifications)=>{
+  const parenttDiv= document.getElementById('phone-details');
+  const childDiv= document.createElement('div');
+
+  //dynamic details window
+  childDiv.innerHTML=
+  `<img class="h-25 w-25" src=${phoneSpecifications.image}></img>
+  <p>Name of phone: ${phoneSpecifications.slug}</p>
+
+  <p><b>Basic features:</b></p>
+  <p><strong>Chipset:</strong> ${phoneSpecifications.mainFeatures.chipSet}</p>
+  <p><strong>Storage:</strong> ${phoneSpecifications.mainFeatures.storage}</p>
+  <p><strong>Display:</strong> ${phoneSpecifications.mainFeatures.displaySize}</p>
+  <p><strong>Sensors:</strong>${phoneSpecifications.mainFeatures.sensors}</p>   
+  <p><strong>Release date:</strong> ${phoneSpecifications.releaseDate}</p>
+
+  <p><b>Other info:</b></p>
+  <p><strong>Bluetooth: </strong>${phoneSpecifications.others.Bluetooth}</p>
+  <p><strong>GPS: </strong>${phoneSpecifications.others.GPS}</p>
+  <p><strong>WLAN: </strong>${phoneSpecifications.others.WLAN}</p>
+
+  `;
+  parenttDiv.appendChild(childDiv);
 }
